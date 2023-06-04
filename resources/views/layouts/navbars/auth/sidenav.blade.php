45361f1982b09016457178d5d98a7fffac961874
ainnajihah173 <ainnajihah173@gmail.com>
2023-06-04 14:22:57 +0800

    1

--- resources/views/layouts/navbars/auth/sidenav.blade.php
+++ resources/views/layouts/navbars/auth/sidenav.blade.php
@@ -24,7 +24,11 @@
                 </a>
             </li>
 
+            
             <!-- Contoh SideNav -->
+            <li class="nav-item mt-3">
+                <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Example</h6>
+            </li>
             <li class="nav-item">
                 <a class="nav-link {{ Route::currentRouteName() == 'user.index' ? 'active' : '' }}"
                     href="{{ route('user.index') }}">
