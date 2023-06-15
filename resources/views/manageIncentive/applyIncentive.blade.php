8789937785c6c46e91f434d63f477a4e9e05802c
Nik Akif Danis bin Sahazerim <103812948+akifszhrm@users.noreply.github.com>
2023-06-15 23:42:50 +0800

    akif

---
+++ resources/views/manageIncentive/applyIncentive.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app', ['class' => 'g-sidenav-show bg-gray-100'])
+
+@section('content')
+    @include('layouts.navbars.auth.topnav', ['title' => 'Kemaskini Maklumat Perkahwinan'])
+    <div class="container-fluid">
+        <div class="row mt-4">
+            <div class="col-lg-12 mb-lg-0 mb-4">
+                <div class="card mb-4">
+                    <div class="card-header pb-0">
+                        <h6>Maklumat Pemohon</h6>
+                    </div>
+                    <div class="card-body p-3">
+                        <!-- Nav Tab -->
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        @include('layouts.footers.auth.footer')
+    </div>
+@endsection
