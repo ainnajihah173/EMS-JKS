bf8746b069a7d2986eeabcc05e0e81bd44a2bafc
Nik Akif Danis bin Sahazerim <103812948+akifszhrm@users.noreply.github.com>
2023-06-16 04:23:22 +0800

    new

--- resources/views/manageIncentive/applyIncentive.blade.php
+++ resources/views/manageIncentive/applyIncentive.blade.php
@@ -1,140 +1,112 @@
 @extends('layouts.app', ['class' => 'g-sidenav-show bg-gray-100'])
 
 @section('content')
-    @include('layouts.navbars.auth.topnav', ['title' => 'Insentif Khas'])
+    @include('layouts.navbars.auth.topnav', ['title' => 'Kemaskini Maklumat Perkahwinan'])
     <div class="container-fluid">
         <div class="row mt-4">
-            <div class="col-lg-12">
-                <div class="card">
+            <div class="col-lg-12 mb-lg-0 mb-4">
+                <div class="card mb-4">
                     <div class="card-header pb-0">
                         <h6>Maklumat Pemohon</h6>
                     </div>
                     <div class="card-body p-3">
                         <!-- Nav Tab -->
-                        <form role="form" method="POST" action="{{ route('applyIncentive:create') }}"
+                        <form role="form" method="POST" action={{ route('applyIncentive:store') }}
                             enctype="multipart/form-data">
                             @csrf
                             <div class="nav-wrapper position-relative end-0">
                                 <ul class="nav nav-pills nav-fill p-1" role="tablist">
                                     <li class="nav-item">
-                                        <a class="nav-link mb-0 px-2 py-1 active" href="#pemohon" role="tab"
-                                            aria-selected="true">
-                                            <span class="fw-bold">Maklumat Pemohon</span>
+                                        <a class="nav-link mb-0 px-0 py-1 active" data-bs-toggle="tab" href="#pemohon"
+                                            role="tab" aria-controls="pemohon" aria-selected="true">
+                                            Maklumat Pemohon
                                         </a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link mb-0 px-2 py-1" href="#pasangan" role="tab"
-                                            aria-selected="false">
-                                            <span class="fw-bold">Maklumat Pasangan</span>
+                                        <a class="nav-link mb-0 px-0 py-1" data-bs-toggle="tab" href="#pasangan"
+                                            role="tab" aria-controls="pasangan" aria-selected="false">
+                                            Maklumat Pasangan
                                         </a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link mb-0 px-2 py-1" href="#waris" role="tab"
-                                            aria-selected="false">
-                                            <span class="fw-bold">Maklumat Waris</span>
+                                        <a class="nav-link mb-0 px-0 py-1" data-bs-toggle="tab" href="#waris"
+                                            role="tab" aria-controls="waris" aria-selected="false">
+                                            Maklumat Waris
                                         </a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="nav-link mb-0 px-2 py-1" href="#dokumen" role="tab"
-                                            aria-selected="false">
-                                            <span class="fw-bold">Dokumen</span>
+                                        <a class="nav-link mb-0 px-0 py-1" data-bs-toggle="tab" href="#dokumen"
+                                            role="tab" aria-controls="dokumen" aria-selected="false">
+                                            Dokumen
                                         </a>
                                     </li>
                                 </ul>
+                                <div class="card-header pb-0 text-center">
+                                    <p class="fw-bold font-italic">** Sila Pastikan Maklumat Diisi dengan Maklumat Yang
+                                        Betul **
+                                    </p>
+                                </div>
                                 <div class="tab-content" id="myTabContent">
-                                    <div class="tab-pane fade show active" id="pemohon" role="tabpanel">
-                                        <div class="row mt-3">
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label for="nama_pemohon" class="form-control-label">Nama Pemohon <span class="text-danger">*</span></label>
-                                                    <input class="form-control" id="nama_pemohon" type="text" name="applicant[app_name]">
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label for="no_kad_pengenalan" class="form-control-label">No. Kad Pengenalan <span class="text-danger">*</span></label>
-                                                    <input class="form-control" id="no_kad_pengenalan" name="applicant[app_ic]" type="text">
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label for="tarikh_lahir" class="form-control-label">Tarikh Lahir <span class="text-danger">*</span></label>
-                                                    <div class="input-group">
-                                                        <input class="form-control" id="tarikh_lahir" name="applicant[tarikh_lahir]" type="date">
-                                                        <span class="input-group-text">
-                                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar2-range" viewBox="0 0 16 16">
-                                                                <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v11a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1H2z"/>
-                                                                <path d="M2.5 4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H3a.5.5 0 0 1-.5-.5V4zM9 8a1 1 0 0 1 1-1h5v2h-5a1 1 0 0 1-1-1zm-8 2h4a1 1 0 1 1 0 2H1v-2z"/>
-                                                            </svg>
-                                                        </span>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label for="umur" class="form-control-label">Umur <span class="text-danger">*</span></label>
-                                                    <input class="form-control" id="umur" name="applicant[umur]" type="number">
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label for="tempat_lahir" class="form-control-label">Tempat Lahir <span class="text-danger">*</span></label>
-                                                    <input class="form-control" id="tempat_lahir" name="applicant[tempat_lahir]" type="text">
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label for="warganegara" class="form-control-label">Warganegara <span class="text-danger">*</span></label>
-                                                    <input class="form-control" id="warganegara" name="applicant[warganegara]" type="text">
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label for="no_passport" class="form-control-label">No. Passport/Tentera/Polis <span class="text-danger">*</span></label>
-                                                    <input class="form-control" id="no_passport" name="applicant[no_passport]" type="text">
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label for="no_telefon" class="form-control-label">No. Telefon <span class="text-danger">*</span></label>
-                                                    <input class="form-control" id="no_telefon" name="applicant[no_telefon]" type="text">
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label for="nama_bank" class="form-control-label">Nama Bank <span class="text-danger">*</span></label>
-                                                    <input class="form-control" id="nama_bank" name="applicant[nama_bank]" type="text">
-                                                </div>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <div class="form-group">
-                                                    <label for="no_akaun_bank" class="form-control-label">No. Akaun Bank <span class="text-danger">*</span></label>
-                                                    <input class="form-control" id="no_akaun_bank" name="applicant[no_akaun_bank]" type="text">
-                                                </div>
-                                            </div>
-                                            <div class="col-md-12">
-                                                <div class="form-group">
-                                                    <label for="alamat_semasa" class="form-control-label">Alamat Semasa <span class="text-danger">*</span></label>
-                                                    <textarea class="form-control" id="alamat_semasa" name="applicant[alamat_semasa]" rows="2"></textarea>
-                                                </div>
-                                            </div>
+                                    <!-- Maklumat Pemohon Tab Pane -->
+                                    <div class="tab-pane fade show active" id="pemohon" role="tabpanel"
+                                        aria-labelledby="home-tab">
+                                        <!-- Replace this section with your content -->
+                                        <div class="text-center mt-4">
+                                            <a href="" class="btn btn-info btn-md ms-4">Seterusnya</a>
                                         </div>
-                                        <div class="form-check mt-2">
-                                            <input class="form-check-input" type="checkbox" value="" id="alamat_sama">
-                                            <label class="form-check-label" for="alamat_sama">
-                                                Tanda Jika Alamat Semasa Sama Dengan Alamat Dalam K/P
-                                            </label>
+                                    </div>
+                                    <!-- Maklumat Pasangan Tab Pane -->
+                                    <div class="tab-pane fade" id="pasangan" role="tabpanel"
+                                        aria-labelledby="profile-tab">
+                                        <!-- Replace this section with your content -->
+                                        <div class="text-center mt-4">
+                                            <a href="" class="btn btn-info btn-md ms-4">Seterusnya</a>
                                         </div>
-                                        <button type="button" class="btn btn-primary mt-3" id="nextBtn">Seterusnya</button>
                                     </div>
-                                    <div class="tab-pane fade" id="pasangan" role="tabpanel">
-                                        <!-- Maklumat Pasangan Form -->
+                                    <!-- Maklumat Waris Tab Pane -->
+                                    <div class="tab-pane fade" id="waris" role="tabpanel"
+                                        aria-labelledby="contact-tab">
+                                        <!-- Replace this section with your content -->
+                                        <div class="text-center mt-4">
+                                            <a href="" class="btn btn-info btn-md ms-4">Seterusnya</a>
+                                        </div>
                                     </div>
-                                    <div class="tab-pane fade" id="waris" role="tabpanel">
-                                        <!-- Maklumat Waris Form -->
-                                    </div>
-                                    <div class="tab-pane fade" id="dokumen" role="tabpanel">
-                                        <!-- Dokumen Form -->
+                                    <!-- Dokumen Tab Pane -->
+                                    <div class="tab-pane fade" id="dokumen" role="tabpanel"
+                                        aria-labelledby="contact-tab">
+                                        <!-- Replace this section with your content -->
+                                        <!-- Note to make sure all fields are filled correctly -->
+<!-- Dokumen Sokongan field -->
+<p>Dokumen Sokongan <span style="color: red;">*</span></p>
+<p>Sila muatnaik salinan kad pengenalan, catatan akad nikah & penyata akaun bank</p>
+
+<!-- Drag & drop file upload section -->
+<h1 class="h4 text-center mb-3">Drag &amp; drop file upload example</h1>
+<form>
+  <fieldset class="upload_dropZone text-center mb-3 p-4">
+    <legend class="visually-hidden">Image uploader</legend>
+    <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" class="bi bi-image" viewBox="0 0 16 16">
+      <path d="M6.002 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
+      <path d="M2.002 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2h12zm12 1a1 1 0 0 1 1 1v6.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12V3a1 1 0 0 1 1-1h12z"/>
+    </svg>
+    <p>Letakkan fail untuk muat naik</p>
+    <p style="font-size: smaller; opacity: 0.7;">atau klik di sini</p>
+    <input id="upload_image_background" data-post-name="image_background" data-post-url="https://someplace.com/image/uploads/backgrounds/" class="position-absolute invisible" type="file" multiple accept="image/jpeg, image/png, image/svg+xml" />
+    <label class="btn btn-upload mb-3" for="upload_image_background">Choose file(s)</label>
+    <div class="upload_gallery d-flex flex-wrap justify-content-center gap-3 mb-0"></div>
+  </fieldset>
+
+  <!-- "+ Tambah Fail" button -->
+  <button type="button" class="btn btn-primary">+ Tambah Fail</button>
+
+  <!-- "Hantar" button -->
+  <button type="submit" class="btn btn-primary">Hantar</button>
+</form>
+
+
+                                        <div class="text-center mt-4">
+                                            <button type="submit" class="btn btn-info btn-md ms-4">Draft</button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -143,5 +115,13 @@
                 </div>
             </div>
         </div>
+        @include('layouts.footers.auth.footer')
     </div>
 @endsection
+
+@push('js')
+    <script>
+        let field = document.querySelector('#dateApply');
+        field.valueAsDate = new Date();
+    </script>
+@endpush
