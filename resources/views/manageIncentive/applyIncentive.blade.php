c3dd3230a9bdb30d1c097546f05f04654120e9e7
Nik Akif Danis bin Sahazerim <103812948+akifszhrm@users.noreply.github.com>
2023-06-16 09:42:00 +0800

    adjust

--- resources/views/manageIncentive/applyIncentive.blade.php
+++ resources/views/manageIncentive/applyIncentive.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app', ['class' => 'g-sidenav-show bg-gray-100'])
 
 @section('content')
-    @include('layouts.navbars.auth.topnav', ['title' => 'Kemaskini Maklumat Perkahwinan'])
+    @include('layouts.navbars.auth.topnav', ['title' => 'Insentif Khas'])
     <div class="container-fluid">
         <div class="row mt-4">
             <div class="col-lg-12 mb-lg-0 mb-4">
